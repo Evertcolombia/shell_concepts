8f736cdfae65074eec27485f11251d640e5793bc
MachinEmmus <emonsalvep38@gmail.com>
2019-11-18 15:33:21 -0500

    Add 1. Read Line usign getline, this is our first prompt

---
+++ shell-wide/shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*Allowed Libraries*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif/*SHELL_H*/
---
+++ shell-wide/simple_shell.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+size_t size = 1024;
+char *buff;
+buff = malloc(size * sizeof(char));
+	if (buff == NULL)
+		return (-1);
+	while (1)
+	{
+		write(1, "MiShellada$ ", 13);
+		getline(&buff, &size, stdin);
+	}
+	free(buff);
+	return (0);
+}
